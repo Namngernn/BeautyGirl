de73a47aa262d26d2d88bd1f8e5d6d7ff9cec7fa
Aphisara Sonthi <nam.apsr.js@gmail.com>
2024-07-15 21:56:36 +0700

    tum dai nid noi na eiei

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,94 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+// import { Outlet } from 'react-router-dom'
+
+import { EditOutlined, EllipsisOutlined, SettingOutlined, HeartOutlined, SearchOutlined } from '@ant-design/icons';
+import { Avatar, Card, Menu, Col, Row } from 'antd';
+
+const { Meta } = Card;
+
 
 const Home = () => {
   return (
-    <div>
-        <h1>home page</h1>
-        </div>
+  <>
+  <Row>
+    <Col xs={12}>
+      <Row start="xs">
+        <Col xs={6} />
+      </Row>
+    </Col>
+  </Row>
+
+
+{/* following + for you */}
+  <div className="bg-white shadow-md">
+      <Menu mode="horizontal" className="alincenter">
+        <Menu.Item className="px-1" level={2}>Following</Menu.Item>
+        <Menu.Item className="px-1" level={2}>For you</Menu.Item>
+        <Menu.Item actions={[<SearchOutlined key="search" />]}></Menu.Item>
+      </Menu>
+    </div>
+
+{/* ประเภทต่างๆ */}
+  <div className="bg-white shadow-md">
+      <Menu mode="horizontal" className="flex-flow: row-reverse nowrap">
+        <Menu.Item className="px-1">All</Menu.Item>
+        <Menu.Item className="px-1">Beauty</Menu.Item>
+        <Menu.Item className="px-1">Fasion</Menu.Item>
+        <Menu.Item className="px-1">Dining</Menu.Item>
+        <Menu.Item className="px-1">Skincare</Menu.Item>
+        <Menu.Item className="px-1">Recipes</Menu.Item>
+
+      </Menu>
+    </div>
+
+
+
+
+{/* โพสต์แต่ละอัน */}
+<div>
+
+  <Card
+    style={{
+      width: 180,
+    }}
+    cover={
+      <img
+      alt="example"
+      src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+      />
+    }
+    actions={[
+      <HeartOutlined key="heart" />,
+    ]}
+    >
+    <Meta
+      avatar={<Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=8" />}
+      title="Card title"
+      description="This is the description"
+      />
+  </Card>
+  <Card
+    style={{
+      width: 180,
+    }}
+    cover={
+      <img
+      alt="example"
+      src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+      />
+    }
+    actions={[
+      <HeartOutlined key="heart" />,
+    ]}
+    >
+    <Meta
+      avatar={<Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=8" />}
+      title="Card title"
+      description="This is the description"
+      />
+  </Card>
+</div>
+  </>
   )
 }
 
--- src/pages/Post/Post.jsx
+++ src/pages/Post/Post.jsx
@@ -1,12 +1,81 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+// import React from 'react'
+// import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { PlusOutlined } from '@ant-design/icons';
+import { Image, Upload } from 'antd';
+
+const getBase64 = (file) =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    // reader.onerror = (error) => reject(error);
+  });
 
 const Post = () => {
+  const [previewOpen, setPreviewOpen] = useState(false);
+  const [previewImage, setPreviewImage] = useState('');
+  const [fileList, setFileList] = useState([
+    {
+      uid: '-1',
+      name: 'image.png',
+      status: 'done',
+      url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+    }
+  ]);
+  const handlePreview = async (file) => {
+    if (!file.url && !file.preview) {
+      file.preview = await getBase64(file.originFileObj);
+    }
+    setPreviewImage(file.url || file.preview);
+    setPreviewOpen(true);
+  };
+  const handleChange = ({ fileList: newFileList }) => setFileList(newFileList);
+  const uploadButton = (
+    <button
+      style={{
+        border: 0,
+        background: 'none',
+      }}
+      type="button"
+    >
+      <PlusOutlined />
+      <div
+        style={{
+          marginTop: 8,
+        }}
+      >
+        Upload
+      </div>
+    </button>
+  );
+
   return (
-    <div>
-      <h1>post page</h1>
-    </div>
-  )
-}
+    <>
+      <Upload
+        action="https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload"
+        listType="picture-card"
+        fileList={fileList}
+        onPreview={handlePreview}
+        onChange={handleChange}
+      >
+        {fileList.length >= 8 ? null : uploadButton}
+      </Upload>
+      {previewImage && (
+        <Image
+          wrapperStyle={{
+            display: 'none',
+          }}
+          preview={{
+            visible: previewOpen,
+            onVisibleChange: (visible) => setPreviewOpen(visible),
+            afterOpenChange: (visible) => !visible && setPreviewImage(''),
+          }}
+          src={previewImage}
+        />
+      )}
+    </>
+  );
+};
 
 export default Post
